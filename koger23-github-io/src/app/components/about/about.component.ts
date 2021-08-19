58a4fbdb8e0cf17a910aceb613736f6323fd1634
koger23 <koger23@gmail.com>
2021-08-19 19:03:54 +0200

    customize About

--- koger23-github-io/src/app/components/about/about.component.ts
+++ koger23-github-io/src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faEnvelope, faGlobe, faGraduationCap, faLayerGroup, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { Utils } from 'src/app/helpers/utils';
 import { GithubService } from 'src/app/services/github.service';
 import { OverlayService } from 'src/app/services/overlay.service';
@@ -11,6 +13,13 @@ import { OverlayService } from 'src/app/services/overlay.service';
 export class AboutComponent implements OnInit {
   contentAboutMe: string;
   contentAboutSite: string;
+  faEnvelope = faEnvelope;
+  faLinkedin = faLinkedin;
+  faLayerGroup = faLayerGroup;
+  faGlobe = faGlobe;
+  faMapMarkedAlt = faMapMarkedAlt;
+  faGithub = faGithub;
+  faGraduationCap = faGraduationCap;
 
   constructor(
     private githubService: GithubService,
